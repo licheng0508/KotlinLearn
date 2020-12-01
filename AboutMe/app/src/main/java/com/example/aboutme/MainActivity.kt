ea1fa6951fa089726b3461737c7eed2ffb88bef3
李城 <licheng1674@stnts.com>
2020-12-01 17:01:07 +0800

    commit

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -8,21 +8,34 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Aleks Haecky")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        /// 按钮点击
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
 
-        /// 昵称点击
-        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
-            updateNickname(it)
-        }
+        /// 按钮点击
+//        findViewById<Button>(R.id.done_button).setOnClickListener {
+//            addNickname(it)
+//        }
+
+//        /// 昵称点击
+//        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+//            updateNickname(it)
+//        }
     }
 
     private fun  addNickname(view: View) {
@@ -30,10 +43,14 @@ class MainActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.nickname_edit)
         val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
+//        binding.nicknameText.text = binding.nicknameEdit.text.toString()
+
+        myName.nickname = editText.text.toString()
+
+        binding.nicknameEdit.visibility = View.GONE
+
         view.visibility = View.GONE
-        nicknameTextView.visibility - View.VISIBLE
+        binding.nicknameText.visibility - View.VISIBLE
 
         /// 隐藏键盘
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
